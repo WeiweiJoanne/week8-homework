d6ff7b7f8bfbf84c59c863d5370e00198b083f6b
WeiweiJoanne <joannewei0921@gmail.com>
2022-05-25 17:01:43 +0800

    feat: 錯誤訊息統一管理

--- app.js
+++ app.js
@@ -24,4 +24,45 @@ app.use('/user', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/post', postsRouter);
 
+app.use(function (req, res, next) {
+  res.status(404).send({
+    status: "false",
+    message: "找不到您的頁面"
+  })
+})
+
+const resErrDev = (err,res) => {
+  
+}
+
+app.use(function (err, req, res, next) {
+  // console.log(err.name);
+  // res.status(500).json({
+  //   "err": err.name
+  // })
+  err.status = err.status || 500;
+
+  if(process.env.NODE_ENV === 'dev'){
+    return resErrDev(err, res)
+  }
+
+
+
+})
+
+// 未捕捉到的 catch 
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('未捕捉到的 rejection：', promise, '原因：', reason);
+  // 記錄於 log 上
+});
+
+process.on('uncaughtException', err => {
+  // 記錄錯誤下來，等到服務都處理完後，停掉該 process
+  console.error('Uncaughted Exception！')
+
+  console.error(err);
+  process.exit(1);
+});
+
+
 module.exports = app;

a31cb32952bd3df2636c901fb1b5c7a0b58d84c9
joannewei <joannewei0921@gmail.com>
2022-05-27 02:04:03 +0800

    feat: 移除try/catch

--- controllers/users.js
+++ controllers/users.js
@@ -3,29 +3,29 @@ const mongoose = require('mongoose')
 const UserModel = require('../models/user')
 
 const handleError = require('../services/handleError')
+const appErr = require('../services/appErr')
 const handleSuccess = require('../services/handleSuccess')
+
 const UserController = {
-  async getUser(req, res) {
+  async getUser(req, res, next) {
     const getUser = await UserModel.find({})
     handleSuccess(res, getUser)
   },
-  async postUser(req, res) {
+  async postUser(req, res, next) {
+    const body = req.body
+    const { name, email, photo } = body
 
-    try {
-      const body = req.body
-      const { name, email, photo } = body
+    if (email === undefined || email.trim() == '') {
+      return appErr(400, '請填寫email', next)
+    }
 
+    const hasUser = await UserModel.findOne({ email: email }).exec()
 
-      const hasUser = await UserModel.findOne({ email: email }).exec()
-
-      if (hasUser == null){
-        const addUser = await UserModel.create({ name, email, photo})
-        handleSuccess(res, addUser)
-      }else{
-        handleError(res)
-      }
-    } catch (err) {
-      handleError(res,err)
+    if (hasUser === null) {
+      const addUser = await UserModel.create({ name, email, photo })
+      handleSuccess(res, addUser)
+    }else{
+      return appErr(400, 'email 重複', next)
     }
 
   }

dda5e7f3a5141dcbd396fc85993ff8a6d21859c6
WeiweiJoanne <joannewei0921@gmail.com>
2022-06-02 15:23:55 +0800

    fix

--- connections/index.js
+++ connections/index.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv')
 
 dotenv.config({ path: 'config.env' })
 
-// const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD)
 
-mongoose.connect('mongodb://localhost:27017/week4')
-// mongoose.connect(DB)
+// mongoose.connect('mongodb://localhost:27017/week4')
+mongoose.connect(DB)
   .then(()=>console.log('資料庫連線成功'))
   .catch(err=> console.log(err))
